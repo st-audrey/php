58d3ba06213b4aa8827f3ad8f600d5cb33021348
Audrey <st-audrey@outlook.fr>
2020-08-27 22:28:10 +0200

    Add apinews on sent cards page

--- js/main.js
+++ js/main.js
@@ -7,22 +7,77 @@ function showCard(id, city, country, lat, lng, img, date) {
     //création d'une row
     var row = document.createElement("DIV");
     row.classList.add("row");
+
     //création d'une row 2
     var row2 = document.createElement("DIV");
     row2.classList.add("row");
 
+    //titre de la carte
     var mapTitle = document.createElement("DIV");
     mapTitle.classList.add("map-title");
     mapTitle.append("Carte postale n°"+id+" - "+city+" ("+country+")");
 
+    //Zone news
+    var newsZone = document.createElement("DIV");
+    newsZone.classList.add("zone-news");
+    newsZone.setAttribute("id", "news");
+
+
+    //Find sources that display news in a specific country. Possible options:
+    //ae ar at au be bg br ca ch cn co cu cz de eg fr gb gr hk hu id ie il in it
+    //jp kr lt lv ma mx my ng nl no nz ph pl pt ro rs ru sa se sg si sk th tr tw ua us ve za .
+    // Default: all countries.
+
+    if (country == "France"){
+        var countryNews = "fr";
+    }else if(country == "Etat-Unis"){
+        var countryNews = "us";
+    }else if (country == "Italie"){
+        var countryNews = "it";
+    }else if (country == "Russie"){
+        var countryNews = "ru";
+    }else if (country == "Chine"){
+        var countryNews = "ch";
+    }else{
+        var countryNews = "us";
+    }
+
+    var url = 'http://newsapi.org/v2/top-headlines?' +
+    'country='+ countryNews +'&' +
+    'apiKey=64793232b58848829439b45b817e9f38';
+
+    console.log(url);
+
+    var req = new Request(url);
+    fetch(req)
+        //.then(function(response) {
+            //console.log(response.json());
+        .then(async response => {
+            const jsonData = await response.json();
+
+            var news = document.getElementById('news');
+            var title = document.createElement('P');
+            var url = document.createElement('A');
+            url.setAttribute("href", jsonData.articles[0].url);
+            url.textContent = "Plus d'infos <"
+
+            title.textContent = jsonData.articles[0].title;
+            news.append(title);
+            news.append(url);
+
+            //jsonData.articles.forEach(article=>{ ... });
+        })
+
+    //zone image
     var imageVille = document.createElement("IMG");
     imageVille.setAttribute("src", "./img/imagesVilles/"+img);
     imageVille.classList.add("img-city");
 
-    //création de la div de la map pays
+    //Zone map pays
     var mapZone = document.createElement("DIV");
     mapZone.setAttribute("id", "map");
-    //création de la div de la map ville
+
+    //zone map ville
     var mapZone2 = document.createElement("DIV");
     mapZone2.setAttribute("id", "map2");
 
@@ -32,6 +87,7 @@ function showCard(id, city, country, lat, lng, img, date) {
     row.appendChild(mapZone);
     row.appendChild(mapZone2);
     row2.appendChild(imageVille);
+    row2.appendChild(newsZone);
 
     var latitude = parseFloat(lat);
     var longitude = parseFloat(lng);
